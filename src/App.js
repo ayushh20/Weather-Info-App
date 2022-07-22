3433346b89860c3e10e534e3c041799a7b70d224
ayushh20 <ayushchaurasia.jmi@gmail.com>
2022-07-22 21:53:06 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=7b28d373a34019d5a5566cb10bcb4f6a
+
+import React from "react";
+import Temp from "./temp.js";
+
+const App = () => {
+  return (
+    <>
+      <Temp />
+    </>
+  );
+};
+
+export default App;
